e6a9955d6ebacf36badca77dd33edc7039b426bf
Nova <115306275+NovaTemplar@users.noreply.github.com>
2023-09-21 12:30:07 +0330

    Update KeySystem.lua

--- KeySystem.lua
+++ KeySystem.lua
@@ -36,7 +36,7 @@ function ScriptS()
     local supported = { 5293755937, 10857813475, 4911733121 }
 
     if table.find(supported, id) then
-      loadstring(game:HttpGet('https://raw.githubusercontent.com/NovaTemplar/ScriptSlayers/main/'..id))()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/NovaTemplar/ScriptSlayers/main/'..id'.lua'))()
     else
       game.Players.LocalPlayer:Kick("🛡️ ScriptSlayers - Invalid Game")
     end
