506891e12187fb6739af06d46abdce6a913f18df
Nova <115306275+NovaTemplar@users.noreply.github.com>
2023-09-21 12:48:42 +0330

    Update and rename Loader to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,7 @@ local id = game.PlaceId
 local supported = { 5293755937, 10857813475, 4911733121 }
 
 if table.find(supported, id) then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/NovaTemplar/ScriptSlayers/main/KeySystem'))()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/NovaTemplar/ScriptSlayers/main/KeySystem.'))()
 else
   game.Players.LocalPlayer:Kick("🛡️ ScriptSlayers - Invalid Game")
 end
